f674b0db56b8cc50c268f61db63a17fde99790d0
Karla Walker <indiablonix@gmail.com>
2026-08-03 22:15:26 -0700

    First update includes: added fonts, uses page, minimal styling and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  adapter: netlify(),
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Karla',
+      cssVariable: '--font-azaret-mono',
+      weights: ['200 800'],
+      styles: ['normal'],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Karla',
+      cssVariable: '--font-heading',
+      weights: ['200 800'],
+      styles: ['normal'],
+    },
+  ],
 });
